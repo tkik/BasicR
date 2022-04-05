5e2cb00638023bbf4a7cce239993404b7dc5d1f7
tkik <tkik@users.noreply.github.com>
2022-04-05 12:21:32 +0000

    Built site for BasicR: 0.0.0.9000@71ecf93

--- doc/assignment.R
+++
@@ -1,3 +0,0 @@
-## ---- echo = FALSE, out.width = "70%", eval=FALSE-----------------------------
-#  knitr::include_graphics("https://github.com/allisonhorst/stats-illustrations/blob/master/rstats-artwork/code_hero.jpg?raw=true", dpi = 100)
-
